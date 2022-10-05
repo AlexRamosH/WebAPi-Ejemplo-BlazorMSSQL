64426d4c7f001aaee172134be71b716c3fce4dd7
Alejandro Ramos <alejandro.ramos8@uabc.edu.mx>
2022-10-05 13:14:03 -0700

    Implementacion sin terminar

---
+++ Client/Services/TodoService.cs
@@ -0,0 +1,32 @@
+﻿using BlazorApp3.Shared;
+
+namespace BlazorApp3.Client.Services
+{
+    public class TodoService : ITodoService
+    {
+        public Task<Todo> Create(Todo todo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Todo>> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Todo> GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Todo> Update(Todo todo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
