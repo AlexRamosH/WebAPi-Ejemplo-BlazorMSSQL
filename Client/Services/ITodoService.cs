41108bd8e5986d0a064aaaf867c08df27aed730a
Alejandro Ramos <alejandro.ramos8@uabc.edu.mx>
2022-10-05 13:12:05 -0700

    Interface services

--- Client/Services/ITodoService.cs
+++ Client/Services/ITodoService.cs
@@ -1,6 +1,13 @@
-﻿namespace BlazorApp3.Client.Services
+﻿using BlazorApp3.Shared;
+
+namespace BlazorApp3.Client.Services
 {
     public interface ITodoService
     {
+        Task<List<Todo>> GetAll();
+        Task<Todo> GetById(int id);
+        Task<bool> Delete(int id);
+        Task<Todo> Create(Todo todo);
+        Task<Todo> Update(Todo todo);
     }
 }
