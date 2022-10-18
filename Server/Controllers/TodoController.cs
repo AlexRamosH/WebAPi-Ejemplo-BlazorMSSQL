37e70b3484a67ba64c0d3cb78070df9422276cb5
alex <alex@gmail.com>
2022-10-17 21:53:36 -0700

    n

--- Server/Controllers/TodoController.cs
+++ Server/Controllers/TodoController.cs
@@ -1,6 +1,44 @@
-﻿namespace BlazorApp3.Server.Controllers
+﻿using BlazorApp3.Server.Repositorios;
+using BlazorApp3.Shared;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorApp3.Server.Controllers
 {
-    public class TodoController
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoController : ControllerBase
     {
+        private readonly ITodoRepository _todoRepository;
+
+        public TodoController(ITodoRepository TodoRepository)
+        {
+            _todoRepository = TodoRepository;
+        }
+
+
+        [HttpPost]
+        public void CreateTodo(Todo tipo)
+        {
+            _todoRepository.CreateTodo(tipo);
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<ServiceResponse<List<Todo>>>> GetTodos()
+        {
+            var result = await _todoRepository.GetAllTodos();
+            return Ok(result);
+        }
+
+
+        [HttpGet("{IdTipo}")]
+        public async Task<ActionResult<ServiceResponse<Todo>>> GetTodoById(int IdTipo)
+        {
+            var result = await _todoRepository.GetTodoById(IdTipo);
+            return Ok(result);
+        }
+
+
+
     }
 }
