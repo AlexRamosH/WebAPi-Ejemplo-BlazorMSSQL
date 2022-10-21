41aa8b0a5746ca1586f0517903a890cbd25a0e98
Alejandro Ramos <alejandro.ramos8@uabc.edu.mx>
2022-10-21 13:24:32 -0700

    comentarios

--- Shared/TodoI.cs
+++ Shared/TodoI.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 
 namespace BlazorApp3.Shared
 {
+    /// <summary>
+    /// Clase base de la tabla TODO en la base de datos
+    /// </summary>
     public partial class TodoI
     {
         public int Id { get; set; }
