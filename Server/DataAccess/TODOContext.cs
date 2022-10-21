c459c182f80cc9ea9a30f31b70c6c56de2044aed
Alejandro Ramos <alejandro.ramos8@uabc.edu.mx>
2022-10-21 13:32:07 -0700

    comentarios

--- Server/DataAccess/TODOContext.cs
+++ Server/DataAccess/TODOContext.cs
@@ -5,6 +5,11 @@ using Microsoft.EntityFrameworkCore.Metadata;
 using BlazorApp3.Shared;
 namespace BlazorApp3.Server.DataAccess
 {
+    /// <summary>
+    /// Realiza las configuraciones nesesarias para mantener la
+    /// relacion con la base de datos, sus tablas y los campos en este.
+    /// 
+    /// </summary>
     public partial class TODOContext : DbContext
     {
         public TODOContext()
@@ -25,6 +30,11 @@ namespace BlazorApp3.Server.DataAccess
 
 
 
+        /// <summary>
+        /// Se crean un modelo por cada tabla que desea ser agregada, dentro de esta
+        /// se definen los capos de la tabla y con que clase y atributo se relaciona
+        /// </summary>
+        /// <param name="modelBuilder"></param>
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<TodoI>(entity =>
